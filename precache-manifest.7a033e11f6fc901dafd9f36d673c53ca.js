b5f4cce99699a666d625721915b7dee818a7d917
Will Macrae <wmacrae@palantir.com>
2021-07-04 20:33:44 -0400

    Updates

---
+++ precache-manifest.7a033e11f6fc901dafd9f36d673c53ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a160ec276f76aa6c698f996a7b6d22",
+    "url": "/mini-deck/index.html"
+  },
+  {
+    "revision": "39a585b91144feaef9cc",
+    "url": "/mini-deck/static/css/main.2f0833a7.chunk.css"
+  },
+  {
+    "revision": "b4171b076c8b2c1d18ff",
+    "url": "/mini-deck/static/js/2.ad6118a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-deck/static/js/2.ad6118a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a585b91144feaef9cc",
+    "url": "/mini-deck/static/js/main.84fb6fce.chunk.js"
+  },
+  {
+    "revision": "efca358c343c38a6853b",
+    "url": "/mini-deck/static/js/runtime-main.a47cd4ce.js"
+  }
+]);
